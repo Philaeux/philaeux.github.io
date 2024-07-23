fd6f54ee7d308ef9dfc598bc452e208882981d82
Vincent 'Philaeux' Lamotte <vinc.lamotte@gmail.com>
2024-07-23 11:16:23 +0000

    Test of RouterLink

--- pblog/src/app/pages/index/index.component.ts
+++ pblog/src/app/pages/index/index.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })
