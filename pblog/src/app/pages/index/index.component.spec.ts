6a4c29a3b7114addca3de072fab7802e58c23a47
Vincent 'Philaeux' Lamotte <vinc.lamotte@gmail.com>
2024-08-07 09:19:16 +0200

    Update index.component.spec.ts

--- pblog/src/app/pages/index/index.component.spec.ts
+++ pblog/src/app/pages/index/index.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 import { IndexComponent } from './index.component';
 
 describe('IndexComponent', () => {
@@ -9,6 +10,12 @@ describe('IndexComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [IndexComponent]
+      providers: [
+        provide: ActivatedRoute,
+        useValue: {
+          params: of({}),
+      }
+     ]
     })
     .compileComponents();
 
