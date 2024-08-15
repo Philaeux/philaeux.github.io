855ed3ac9e1d4bfab7cafb9e518fa88066ebd6f6
Vincent Philaeux LAMOTTE <vinc.lamotte@gmail.com>
2024-08-16 01:06:02 +0200

    Start of new design

--- pblog/src/app/app.component.ts
+++ pblog/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterLink,
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
