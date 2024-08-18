d068aaa2267414adc1a5a05e79404c86ce6f58ee
Vincent 'Philaeux' Lamotte <vinc.lamotte@gmail.com>
2024-08-18 23:11:53 +0000

    Menu with buttons

--- pblog/src/app/app.component.ts
+++ pblog/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterLink,
-    RouterOutlet
+    RouterOutlet,
+    MatMenuModule,
+    MatButtonModule,
+    CommonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
